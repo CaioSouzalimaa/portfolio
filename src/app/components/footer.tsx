16a49d273631538928aab83da56854472e2c1abf
Caio Souza Lima <caiosouzalima2002@gmail.com>
2023-09-17 15:24:12 -0300

    fix: footer width

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,9 +1,9 @@
 export const Footer = () => {
   return(
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent border-b-transparent text-white">
-      <div className="container p-14 flex justify-between">
+      <div className="p-14 flex justify-between">
         <span>LOGO</span>
-        <p className="text-slate-600">All rights reserved.</p>
+        <p className="text-slate-400">Todos os direitos reservados.</p>
       </div>
     </footer>
   )
