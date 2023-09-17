b05abf274a83883ba36f301bc375f0eea5870b6d
Caio Souza Lima <caiosouzalima2002@gmail.com>
2023-09-17 14:41:46 -0300

    hotfix: remove resend

--- src/app/api/send/route.tsx
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-export async function POST() {
- //TODO: Implement send email
-}
