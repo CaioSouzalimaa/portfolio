8d0dac927e94e56b5b59383eb87cc4ac0694fed3
Caio Souza Lima <caiosouzalima2002@gmail.com>
2023-09-17 14:38:11 -0300

    hotfix: remove unused implementation

--- src/app/api/send/route.tsx
+++ src/app/api/send/route.tsx
@@ -4,18 +4,5 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: 'Caio <caiomic2002@gmail.com>',
-      to: ['caiomic2002@gmail.com'],
-      subject: 'Hello world',
-      react: <>
-        <p>Email Body</p>
-      </>,
-    });
-
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
+ //TODO: Implement send email
 }
